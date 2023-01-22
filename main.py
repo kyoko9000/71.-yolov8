0730e9f1304fc0e3d4aaf230086a2d1a84914e80
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-01-23 06:10:16 +0700

    ""

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 import cv2
 from ultralytics import YOLO
-
-# Load a model
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-
-# Use the model
-results = model("video.mp4", return_outputs=True, show=True)  # predict on an image
-
-for result, frame in results:
-    print(frame)
-    if result:
-        for item in result["det"]:
-            print("location", item[0:4])
-            print("accurate", item[4])
-            print("class", item[5])
-
-    cv2.imshow("show", frame)
-    cv2.waitKey(1)  # 1 millisecond
+results = model("video.mp4", show=True)  # List of Results objects
+print(len(results))
+# for _ in results:
+#     print(results.boxes)
+#     cv2.imshow("show", frame)
+#     cv2.waitKey(1)  # 1 millisecond
