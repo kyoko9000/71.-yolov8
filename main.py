a0ba537ceba848ad68d3218185699964097154cc
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-01-23 06:48:00 +0700

    update main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import cv2
 from ultralytics import YOLO
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-results = model("video.mp4", show=True)  # List of Results objects
-print(len(results))
-# for _ in results:
-#     print(results.boxes)
+results = model("video.mp4", stream=True)  # List of Results objects
+
+for result in results:
+    # print(result.numpy())
+    for box in result:
+        print(box.numpy())
 #     cv2.imshow("show", frame)
 #     cv2.waitKey(1)  # 1 millisecond 111222
