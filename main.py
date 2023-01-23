b17faee40393836893a3cd6552969d40f8bc3d34
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-01-23 09:45:23 +0700

    update main

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import cv2
 from ultralytics import YOLO
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-results = model("video.mp4", stream=True)  # List of Results objects
 
-for result in results:
-    # print(result.numpy())
-    for box in result:
-        print(box.numpy())
-#     cv2.imshow("show", frame)
-#     cv2.waitKey(1)  # 1 millisecond 111222
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+results = model("0", show=True, stream=True)  # List of Results objects
+
+
+# for result in results:
+#     boxes = result.boxes.numpy()  # Boxes object for bbox outputs
+#     for box in boxes:  # there could be more than one detection
+#         print("class", box.cls)
+#         print("xyxy", box.xyxy[0])
+#         print("conf", box.conf)
+
+def show_frame():
+    cv2.imshow("show", frame)
+    cv2.waitKey(1)
+
+
+for result, frame in results:
+    show_frame()
+    boxes = result[0].boxes.numpy()  # Boxes object for bbox outputs
+    for box in boxes:  # there could be more than one detection
+        print("class", box.cls)
+        print("xyxy", box.xyxy)
+        print("conf", box.conf)
