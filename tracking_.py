9e629e696465ae0e1b9252e89257888227e70ef8
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-03-07 20:01:45 +0700

    update main

--- tracking_.py
+++ tracking_.py
@@ -2,14 +2,13 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")  # or a segmentation model .i.e yolov8n-seg.pt
 results = model.track(source="video.mp4",
-                      # stream=True,
+                      stream=True,
                       show=True,
                       tracker="bytetrack.yaml")
 
 for result in results:
-    print(result)
-    # boxes = result[0].boxes.numpy()  # Boxes object for bbox outputs
-    # for box in boxes:  # there could be more than one detection
-    #     print("class", box.cls)
-    #     print("xyxy", box.xyxy)
-    #     print("conf", box.conf)
+    boxes = result[0].boxes.numpy()  # Boxes object for bbox outputs
+    for box in boxes:  # there could be more than one detection
+        print("class", box.cls)
+        print("xyxy", box.xyxy)
+        print("conf", box.conf)
